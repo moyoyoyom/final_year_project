e4e691ea8c1f115eeab8e9ca86b15b314263282b
moyoyoyom <128235845+moyoyoyom@users.noreply.github.com>
2025-01-14 10:47:30 +0000

    Added buttons

--- gourmet-buddy/view/UI/components/Icons.js
+++ gourmet-buddy/view/UI/components/Icons.js
@@ -4,9 +4,11 @@ const Icons = {};
 
 const ReturnIcon = () => <Ionicons name="chevron-back" size={30} />;
 const MenuOptionsIcon = () => <Ionicons name="options-outline" size={30} />;
+const CameraIcon = () => <Ionicons name="camera-outline" size={30} />;
 
 //Compose
 Icons.ReturnIcon = ReturnIcon;
 Icons.MenuOptionsIcon = MenuOptionsIcon;
+Icons.CameraIcon = CameraIcon;
 
 export default Icons;
--- gourmet-buddy/view/screens/SearchScreen.js
+++ gourmet-buddy/view/screens/SearchScreen.js
@@ -3,6 +3,7 @@ import { Header } from "react-native-elements";
 import Screen from "../UI/Screen";
 import SearchField from "../UI/components/SearchField";
 import Icons from "../UI/components/Icons";
+import Button from "../UI/components/Button";
 
 const SearchScreen = () => {
   //View
@@ -12,6 +13,10 @@ const SearchScreen = () => {
         leftComponent={<Icons.ReturnIcon />}
         centerComponent={<SearchField />}
         rightComponent={<Icons.MenuOptionsIcon />}
+      />
+      <Button
+        buttonText="Scan Barcode or Label"
+        buttonIcon={<Icons.CameraIcon />}
       />
     </Screen>
   );
