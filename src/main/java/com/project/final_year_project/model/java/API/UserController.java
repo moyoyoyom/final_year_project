cedb41ad86bd070209fec25b3d4a4702a90b0572
moyoyoyom <128235845+moyoyoyom@users.noreply.github.com>
2025-02-02 00:17:59 +0000

    Some changes before i switch databases

--- src/main/java/com/project/final_year_project/model/java/API/UserController.java
+++ src/main/java/com/project/final_year_project/model/java/API/UserController.java
@@ -5,13 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.final_year_project.model.java.data.User;
 import com.project.final_year_project.model.java.service.UserService;
 
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
 
