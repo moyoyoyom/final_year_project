43573a044efb0dc351d1ac4cacbc0ed5776fb282
moyoyoyom <128235845+moyoyoyom@users.noreply.github.com>
2025-03-28 22:13:47 +0000

    Got food product detail screen to work again

--- gourmet-buddy/presenter/FoodProductDetailsScreenPreseneter.js
+++ gourmet-buddy/presenter/FoodProductDetailsScreenPreseneter.js
@@ -2,20 +2,22 @@ import { useContext } from "react";
 import FoodProductDetailsScreen from "../view/screens/FoodProductDetailsScreen";
 import { AuthenticationContext } from "../model/AuthenicationContext";
 import useLoad from "../model/useLoad";
-import LoadingScreen from "../view/screens/LoadingScreen";
 
 const FoodProductDetailsScreenPresenter = ({ navigation, route }) => {
   //Initialisations
   const { user } = useContext(AuthenticationContext);
-  console.log(user.userID);
   const { foodProduct } = route.params;
   const userSensitivitiesEndpoint = `http://192.168.1.253:8090/api/relationships/cannoteat/${user.userID}`;
 
   //State
-  const [userSensitivities] = useLoad(userSensitivitiesEndpoint);
+  const [userSensitivities, isUserSensitivitiesLoading] = useLoad(
+    userSensitivitiesEndpoint
+  );
+
+  if (isUserSensitivitiesLoading) return;
   console.log(userSensitivities);
 
-  const formattedSensitivities = userSensitivities.result.map(
+  const formattedSensitivities = userSensitivities.map(
     (trigger) => trigger.triggerName
   );
 
@@ -23,10 +25,6 @@ const FoodProductDetailsScreenPresenter = ({ navigation, route }) => {
   const handleBackClick = () => {
     navigation.navigate("SearchScreen");
   };
-
-  if (!decodedUser) {
-    return <LoadingScreen />;
-  }
 
   //View
   return (
