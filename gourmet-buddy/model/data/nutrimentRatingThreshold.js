fceb77516a3b36b99c185943a8a0994dcb61d2d0
moyoyoyom <128235845+moyoyoyom@users.noreply.github.com>
2025-04-01 14:13:07 +0100

    Made a rating threshold to compare nutriments against

---
+++ gourmet-buddy/model/data/nutrimentRatingThreshold.js
@@ -0,0 +1,10 @@
+const nutrimentRatingThreshold = {
+  carbohydratePer100g: { goodInHighAmounts: true, low: 10, high: 50 },
+  caloriesPer100g: { goodInHighAmounts: false, low: 100, high: 400 },
+  fatPer100g: { goodInHighAmounts: false, low: 3, high: 17.5 },
+  saturatedFatPer100g: { goodInHighAmounts: false, low: 1.5, high: 6 },
+  proteinPer100g: { goodInHighAmounts: true, low: 5, high: 20 },
+  saltPer100g: { goodInHighAmounts: false, low: 0.3, high: 0.6 },
+  sodiumPer100g: { goodInHighAmounts: false, low: 0.1, high: 0.6 },
+  sugar: { goodInHighAmounts: false, low: 5, high: 22.5 },
+};
