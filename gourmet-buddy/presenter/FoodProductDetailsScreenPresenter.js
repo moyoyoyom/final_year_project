b8ec1ae008400e8568e9bb799a7811c1b828856b
moyoyoyom <128235845+moyoyoyom@users.noreply.github.com>
2025-04-07 19:36:11 +0100

    Saving and liking works but food products cannot be saved and liked

--- gourmet-buddy/presenter/FoodProductDetailsScreenPresenter.js
+++ gourmet-buddy/presenter/FoodProductDetailsScreenPresenter.js
@@ -3,12 +3,14 @@ import FoodProductDetailsScreen from "../view/screens/FoodProductDetailsScreen";
 import { AuthenticationContext } from "../model/AuthenicationContext";
 import useLoad from "../model/useLoad";
 import API from "../model/API";
+import { Alert } from "react-native";
 
 const FoodProductDetailsScreenPresenter = ({ navigation, route }) => {
   //Initialisations
   const { user } = useContext(AuthenticationContext);
   const { foodProduct } = route.params;
   const userSensitivitiesEndpoint = `http://192.168.1.253:8090/api/relationships/cannoteat/${user.userID}`;
+  const ratingEndpoint = "http://192.168.1.253:8090/api/rating/save";
 
   //State
   const [userSensitivities, isUserSensitivitiesLoading] = useLoad(
@@ -32,19 +34,39 @@ const FoodProductDetailsScreenPresenter = ({ navigation, route }) => {
 
   const handleLikeClick = async () => {
     const response = await API.post(ratingEndpoint, {
-      userFoodProductRatingID: {
-        userID: user.userID,
-        foodProduct: foodProduct.result.code,
-      },
       user: {
         userID: user.userID,
       },
       foodProduct: {
-        foodProduct: foodProduct,
+        code: foodProduct.result.code,
       },
+      rating: "LIKED",
     });
+    if (response.isSuccess) {
+      console.log("Successful");
+    } else {
+      Alert.alert(
+        "There have been issues rating this product, try again later."
+      );
+    }
   };
-  const handleSaveClick = () => {};
+  const handleSaveClick = async () => {
+    const response = await API.post(ratingEndpoint, {
+      user: {
+        userID: user.userID,
+      },
+      foodProduct: {
+        code: foodProduct.result.code,
+      },
+      rating: "SAVED",
+    });
+    if (response.isSuccess) {
+    } else {
+      Alert.alert(
+        "There have been issues rating this product, try again later."
+      );
+    }
+  };
 
   //View
   return (
@@ -53,6 +75,8 @@ const FoodProductDetailsScreenPresenter = ({ navigation, route }) => {
       userSensitivities={formattedSensitivities}
       onBackClick={handleBackClick}
       onLearnMoreClick={handleLearnMoreClick}
+      onLikeClick={handleLikeClick}
+      onSaveClick={handleSaveClick}
     />
   );
 };
