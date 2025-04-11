6651a9c2e71ed0f413f9112bff2429f79fa5a6e5
moyoyoyom <128235845+moyoyoyom@users.noreply.github.com>
2025-04-11 23:46:08 +0100

    Fixed issue where food triggers would not initially show

--- gourmet-buddy/presenter/DislikeProfilePresenter.js
+++ gourmet-buddy/presenter/DislikeProfilePresenter.js
@@ -1,42 +1,27 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import IntoleranceProfileScreen from "../view/screens/IntoleranceProfileScreen";
 import { Alert } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { jwtDecode } from "jwt-decode";
 import useLoad from "../model/useLoad";
+import { AuthenticationContext } from "../model/AuthenicationContext";
 
 const DislikeProfilePresenter = ({ navigation }) => {
   //Initialisations
+  const { user } = useContext(AuthenticationContext);
   const foodTriggerEndpoint =
     "http://gourmet-buddy-app.eu-west-2.elasticbeanstalk.com/api/foodtriggers/getallbygroups";
   const userRelationshipsEndpoint =
     "http://gourmet-buddy-app.eu-west-2.elasticbeanstalk.com/api/relationships/save";
-  const getFoodTriggersOptions = {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  };
   const screenType = "dislikeProfile";
-
-  useEffect(() => {
-    const findUserToken = async () => {
-      try {
-        const token = await AsyncStorage.getItem("userToken");
-        setUserToken(token);
-      } catch (error) {
-        Alert.error(error);
-      }
-    };
-    findUserToken();
-  }, []);
 
   //State
   const [foodTriggers, isFoodTriggersLoading] = useLoad(foodTriggerEndpoint);
   const [userFoodTriggers, setUserFoodTriggers] = useState([]);
-  const [userToken, setUserToken] = useState(null);
   const [selectedItems, setSelectedItems] = useState([]);
-  const [filteredFoodTriggers, setFilteredFoodTriggers] = useState([
-    foodTriggers,
-  ]);
+  const [filteredFoodTriggers, setFilteredFoodTriggers] = useState([]);
+
+  useEffect(() => {
+    setFilteredFoodTriggers(foodTriggers);
+  }, [isFoodTriggersLoading]);
 
   //Handlers
   const onTriggerSelect = (selectedTrigger) => {
@@ -56,7 +41,6 @@ const DislikeProfilePresenter = ({ navigation }) => {
   };
 
   const postEachUserRelationship = async () => {
-    const decodedUser = jwtDecode(userToken);
     userFoodTriggers.forEach(async (foodTrigger) => {
       try {
         await fetch(userRelationshipsEndpoint, {
@@ -64,12 +48,12 @@ const DislikeProfilePresenter = ({ navigation }) => {
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
             userIngredientRelationshipID: {
-              userID: decodedUser.userID,
+              userID: user.userID,
               foodTriggerID: foodTrigger.foodTriggerID,
             },
             relationship: "DISLIKES",
             user: {
-              userID: decodedUser.userID,
+              userID: user.userID,
             },
             foodTrigger: {
               foodTriggerID: foodTrigger.foodTriggerID,
